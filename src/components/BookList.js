c474ca8b4df2a55994bd0c918a5dd8a152387565
sandesh poudel <sandesh100poudel@gmail.com>
2023-10-04 22:06:20 +0545

    simple example of contextProvider

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,16 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 function BookList({books,onDelete,onEdit}){
+
+    const {count,incrementCount} = useContext(BooksContext)
 
     const renderList = books.map((book)=>{
         return( <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>)
     })
     return(
-        <div>{renderList}</div>
+        <div>{count}<button onClick={incrementCount}></button>{renderList}</div>
     )
 }
 export default BookList;
